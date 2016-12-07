0cceb6a523dd5bc2292476955ddd3072ab1d1d68
Max Ahnen <m.knoetig@gmail.com>
2016-12-07 12:48:25 +0100

    initial commit with structure

---
+++ acp_paper_analysis/__main__.py
@@ -0,0 +1,8 @@
+def main():
+    '''
+    The main.
+    '''
+    return
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='acp_paper_analysis',
+    version='0.1',
+    description='High level analysis & plotting methods for first ACP paper',
+    url='https://github.com/TheBigLebowSky/acp_paper_analysis',
+    author='Max Ahnen',
+    author_email='m.knoetig@gmail.com',
+    licence='MIT',
+    packages=[
+        'acp_paper_analysis'
+    ],
+    package_data={'acp_paper_analysis': ['resources/*']},
+    entry_points={
+        'console_scripts': [
+            'acp_paper_analysis = acp_paper_analysis.__main__:main'
+        ]
+    },
+    install_requires={
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'docopt',
+        'gamma_limits_sensitivity'
+    },
+    dependency_links={
+        'git+ssh://git@github.com/mahnen/gamma_limits_sensitivity.git#egg=gamma_limits_sensitivity',
+    },
+    tests_require=['pytest']
+)
