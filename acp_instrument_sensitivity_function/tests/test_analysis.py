64a000d0fb4b4ef7f4483a2bf8a292a7dfe98095
sebastian <sebmuell@phys.ethz.ch>
2017-07-12 18:00:05 +0200

    remove unused code

--- acp_instrument_sensitivity_function/tests/test_analysis.py
+++ acp_instrument_sensitivity_function/tests/test_analysis.py
@@ -65,28 +65,6 @@ def test_get_gamma_effective_area_figure():
     assert isinstance(effective_area_figure, matplotlib.figure.Figure)
 
 
-# def test_analysis():
-#     '''
-#     This test checks if the analysis does
-#     make sense.
-#     '''
-#     result_dict = isf.analysis(
-#         isf.__path__[0] + '/resources/test_infolder/',
-#         is_test=True
-#         )
-
-#     for plot_name in result_dict['plots']:
-#         assert isinstance(
-#             result_dict['plots'][plot_name], matplotlib.figure.Figure
-#             )
-
-#     for data_name in result_dict['data']:
-#         assert isinstance(
-#             result_dict['data'][data_name], numpy.ndarray
-#             )
-
-
-
 def test_get_time_to_detections():
     '''
     This test checks if the time to detectin method is working
