4566140c427502787701db7ae16fda7381f5f233
sebastian <sebmuell@phys.ethz.ch>
2017-07-12 18:06:29 +0200

    obsolete and later imported anyhow

--- acp_instrument_sensitivity_function/__init__.py
+++ acp_instrument_sensitivity_function/__init__.py
@@ -1,9 +1,6 @@
 '''
 This is the hard working code in order to create publication plots
 '''
-# import gamma_limits_sensitivity as gls
-#
-# import matplotlib.pyplot as plt
 import os
 from os.path import join
 import csv
